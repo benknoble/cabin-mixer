b2533d8b0db01eafefcd75eec48e5e9061d15faf
D. Ben Knoble <ben.knoble+github@gmail.com>
2026-03-07 14:21:47 -0500

    gui: extract mixer window as a procedure

--- gui/mixer.rkt
+++ gui/mixer.rkt
@@ -16,67 +16,9 @@
    Maybe "field toggles" should be a "preferences" window?
 
   |#
-  (define/obs @chart (match (df-series-names data)
-                       [(cons x _) x]
-                       [_ #f]))
-  (define/obs @style 'count)
   (plot-pen-color-map 'tab20)
   (plot-brush-color-map 'tab20)
-  (void
-   (render
-    (window
-     #:title "Cabin Mixer"
-     (choice '(count proportion)
-             #:choice->label ~a
-             #:selection @style
-             (λ (new-choice)
-               (:= @style (or new-choice 'count))))
-     (tabs
-      (df-series-names data)
-      (λ (e _choices current)
-        (case e [(select) (:= @chart current)]))
-      (observable-view
-       ;; NB data may be observable here, too. Should probably use a single
-       ;; @state or obs-combine them here
-       (obs-combine vector @chart @style)
-       (match-lambda
-         [(vector chart style)
-          (cond
-            [chart
-             (snip
-              data
-              (λ (data w h)
-                (define label (add-labels! data chart))
-                (define the-data (~> (data) (df-select* "Cabin" label) vector->list (map vector->list _)))
-                (define the-labels (~> (data) (df-select label)
-                                       vector->list sep set set->list
-                                       (sort string<?)))
-                (define count-data
-                  (sort (for/list ([group (group-by first the-data)])
-                          (define cabin (first (first group)))
-                          (define counter
-                            (for/fold ([acc (hash)])
-                                      ([value (map second group)])
-                              (hash-update acc value add1 0)))
-                          (list cabin
-                                (for/list ([label the-labels])
-                                  (hash-ref counter label 0))))
-                        string<?
-                        #:key {~> first ~a}))
-                (define chart-data
-                  (case style
-                    [(count) count-data]
-                    [(proportion)
-                     (for/list ([group count-data])
-                       (define cabin (first group))
-                       (define counts (second group))
-                       (define total (apply + counts))
-                       (list cabin (map {(/ total)} counts)))]))
-                (plot-snip
-                 #:width w
-                 #:height h
-                 (stacked-histogram chart-data #:labels the-labels))))]
-            [else (text "Please select a chart from the list of tabs.")])])))))))
+  (void (render (mixer data))))
 
 (require racket/gui/easy
          frosthaven-manager/curlique
@@ -86,6 +28,65 @@
          plot/snip
          (prefix-in pict: pict)
          data-frame)
+
+(define (mixer data)
+  (define/obs @chart (match (df-series-names data)
+                       [(cons x _) x]
+                       [_ #f]))
+  (define/obs @style 'count)
+  (window
+   #:title "Cabin Mixer"
+   (choice '(count proportion)
+           #:choice->label ~a
+           #:selection @style
+           (λ (new-choice)
+             (:= @style (or new-choice 'count))))
+   (tabs
+    (df-series-names data)
+    (λ (e _choices current)
+      (case e [(select) (:= @chart current)]))
+    (observable-view
+     ;; NB data may be observable here, too. Should probably use a single
+     ;; @state or obs-combine them here
+     (obs-combine vector @chart @style)
+     (match-lambda
+       [(vector chart style)
+        (cond
+          [chart
+           (snip
+            data
+            (λ (data w h)
+              (define label (add-labels! data chart))
+              (define the-data (~> (data) (df-select* "Cabin" label) vector->list (map vector->list _)))
+              (define the-labels (~> (data) (df-select label)
+                                     vector->list sep set set->list
+                                     (sort string<?)))
+              (define count-data
+                (sort (for/list ([group (group-by first the-data)])
+                        (define cabin (first (first group)))
+                        (define counter
+                          (for/fold ([acc (hash)])
+                                    ([value (map second group)])
+                            (hash-update acc value add1 0)))
+                        (list cabin
+                              (for/list ([label the-labels])
+                                (hash-ref counter label 0))))
+                      string<?
+                      #:key {~> first ~a}))
+              (define chart-data
+                (case style
+                  [(count) count-data]
+                  [(proportion)
+                   (for/list ([group count-data])
+                     (define cabin (first group))
+                     (define counts (second group))
+                     (define total (apply + counts))
+                     (list cabin (map {(/ total)} counts)))]))
+              (plot-snip
+               #:width w
+               #:height h
+               (stacked-histogram chart-data #:labels the-labels))))]
+          [else (text "Please select a chart from the list of tabs.")])])))))
 
 (define (delete-current-assignments! df)
   (for ([series (df-series-names df)]
